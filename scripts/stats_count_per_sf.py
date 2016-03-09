02db207b580289ff174ce1f684d9d00e1e974d11
Marieke van Erp <marieke.van.erp@vu.nl>
2016-03-09 22:51:06 +0100

    first attempt at dominance scoring, but it's ugly!

--- scripts/stats_count_per_sf.py
+++ scripts/stats_count_per_sf.py
@@ -8,6 +8,7 @@
 # created by: marieke.van.erp@vu.nl
 # 9 March 2016
 
+from __future__ import division
 import sys
 import json
 import pprint
@@ -20,25 +21,67 @@ with open(sys.argv[1]) as data_file:
 
 total = 0 
 number_of_resources = {}
+link_combo_frequency = {}
 for item in data:
-#	print item, len(data[item])
+#	print item, len(data[item]), data[item]
 	total=+1 
 	if len(data[item]) in number_of_resources:
 		number_of_resources[len(data[item])] = number_of_resources[len(data[item])] + 1 
 	else:
 		number_of_resources[len(data[item])] = 1 
+	for link in data[item]:
+	#	print "link combo", item, link, data[item][link]
+		if item in link_combo_frequency:
+			if link in link_combo_frequency[item]:
+				link_combo_frequency[item][link] = link_combo_frequency[item][link] + int(data[item][link])
+			else:
+				link_combo_frequency[item][link] = int(data[item][link])
+		else:
+			link_combo_frequency[item] = {}
+			link_combo_frequency[item][link]= int(data[item][link]) 
 
 meanings = 0
 forms = 0
-data = []
+confus_data = []
 for i in number_of_resources:
 	print i, number_of_resources[i]
 	#meanings = meanings + (int(i) * int(number_of_resources[i]))
 	#forms = forms + int(number_of_resources[i])
 	for x in range (0, int(number_of_resources[i])):
 		#print x, int(number_of_resources[i])
-		data.append(int(i))
+		confus_data.append(int(i))
 		 
 #print data 
-print "mean: ", "%.2f" % statistics.mean(data)
-print  "stdev: ", "%.2f" % statistics.stdev(data)  	
+print "confusability mean: ", "%.2f" % statistics.mean(confus_data)
+print  "confusability stdev: ", "%.2f" % statistics.stdev(confus_data)  	
+
+dom_data = [] 
+max = 0 
+min = 1000  
+for x in link_combo_frequency:
+	total = 0 
+	local_max = 0 
+	for y in link_combo_frequency[x]:
+	#	print x, y, link_combo_frequency[x][y], len(link_combo_frequency[x])
+		total = total + int(link_combo_frequency[x][y])
+		if int(link_combo_frequency[x][y]) > max:
+			max = int(link_combo_frequency[x][y])
+	#		print x, y, link_combo_frequency[x][y], len(link_combo_frequency[x])
+	#		print "whoop whoop"
+		if int(link_combo_frequency[x][y]) < min:
+			min = int(link_combo_frequency[x][y])
+		if int(link_combo_frequency[x][y]) > local_max:
+			local_max = int(link_combo_frequency[x][y])
+	dom_per_sf =  local_max / total
+#	print "total, local_max, dom_per_sf", x, y, total, local_max, dom_per_sf
+	dom_data.append(dom_per_sf)	
+#	print idx, wikilinks_frequency[val]
+#	for x in range (0, wikilinks_frequency[val]):
+#		dom_data.append(wikilinks_frequency[val]) 
+
+
+
+#print dom_data
+print "dominance mean: ", "%.2f" % statistics.mean(dom_data)
+print  "dominance stdev: ", "%.2f" % statistics.stdev(dom_data)
+print "max/min", max, min
